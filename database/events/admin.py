5814bff66fea38152521cf50887dea54b40cf738
lmc <lmc2151@icloud.com>
2024-03-22 19:52:17 +0900

    chore: Migrate models
    - Make migrations and migrate
    - Install pillow

--- database/events/admin.py
+++ database/events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event, Participant, Vote, VoteResponse, VoteResult
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Participant)
+admin.site.register(Vote)
+admin.site.register(VoteResponse)
+admin.site.register(VoteResult)
